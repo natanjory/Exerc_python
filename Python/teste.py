5aa225093021b04c5e16bf97292ea0f8e9524cbb
Natan Jory <natan.jory@gmail.com>
2017-03-08 19:00:11 +0000

    Primeiros arquivos

---
+++ Python/teste.py
@@ -0,0 +1,15 @@
+transp1 = 3
+transp2 = 0.75
+
+livro = input('Entre com o preço do livro: ')
+ncopias = input('Insira o número de cópias desejadas ')
+
+print(type(livro))
+
+desconto = (float(livro) / 100.0 * 40)
+#print(desconto)
+
+custo = ((float(livro) - desconto)*float(ncopias)) + transp1 + ((float(ncopias)-1)*transp2)
+
+#custo = (float(livro)-float(desconto))+float(transp1)+((float(ncopias)-1)*float(transp2))
+print("O valor final da venda é R$",custo)
